3eb2814504f56b125577cb05fa41f5148b06939a
@ <@>
2022-01-28 13:38:25 +0200

    Camera zooms out with tiles sliding in

--- Assets/Scripts/BoardMgr.cs
+++ Assets/Scripts/BoardMgr.cs
@@ -10,6 +10,8 @@ public class BoardMgr : MonoBehaviour {
 	[SerializeField] private IntRef m_worldSizeIncrease;
 
 	[SerializeField] private float m_tilesSlideInSpeed;
+	[SerializeField] private float m_cameraZoomOutSpeed;
+	[SerializeField] private AnimationCurve m_cameraZoomOutCurve;
 
 	private GameObject[,] m_tiles;
 
@@ -38,7 +40,7 @@ public class BoardMgr : MonoBehaviour {
 		}else{
 			diff = m_worldMaxSize - m_worldCurrentSize;
 			if(diff % 2 != 0 || diff / 2 == 0) return;
-			m_worldCurrentSize = m_worldMaxSize;
+			m_worldCurrentSize.SetValue(m_worldMaxSize);
 		}
 
 		GameObject[,] newBoard = new GameObject[m_worldCurrentSize, m_worldCurrentSize];
@@ -51,12 +53,12 @@ public class BoardMgr : MonoBehaviour {
 			for(int j = 0; j < m_worldCurrentSize; j++){
 				if((i < diff || i > m_worldCurrentSize - 1 - diff) || (j < diff || j > m_worldCurrentSize - 1 - diff)){
 					tilePosition = m_startPosition + new Vector3(i, -j ,0);
-					GameObject newTile = Instantiate<GameObject>(m_tilePrefab, tilePosition * 10, Quaternion.identity, this.transform);
+					GameObject newTile = Instantiate<GameObject>(m_tilePrefab, tilePosition * 5, Quaternion.identity, this.transform);
 					newTile.name = $"Tile_{i}x{j}";
 					newBoard[i, j] = newTile;
 					newTiles.Add(newTile);
 					newTilesTargetPos.Add(tilePosition);
-					newTilesCurrentPos.Add(tilePosition * 10);
+					newTilesCurrentPos.Add(tilePosition * 5);
 				}else{
 					newBoard[i, j] = m_tiles[i - diff, j - diff];
 					newBoard[i, j].name = $"Tile_{i}x{j}";
@@ -65,7 +67,8 @@ public class BoardMgr : MonoBehaviour {
 			}
 		}
 		m_tiles = newBoard;
-		StartCoroutine(SlideTilesIn(newTiles, newTilesCurrentPos, newTilesTargetPos));
+		//StartCoroutine(SlideTilesIn(newTiles, newTilesCurrentPos, newTilesTargetPos));
+		StartCoroutine(SlideAndZoom(newTiles, newTilesCurrentPos, newTilesTargetPos));
 	}
 
 	void Start(){
@@ -75,20 +78,62 @@ public class BoardMgr : MonoBehaviour {
 	void Update(){
 		if(Input.GetKeyDown(KeyCode.Space)){
 			IncreaseBoard();
+			/*if(m_worldCurrentSize > Camera.main.orthographicSize * 2){
+				StartCoroutine(ZoomOutCamera(m_worldCurrentSize/2));
+			}*/
 		}
 	}
 
 	IEnumerator SlideTilesIn(List<GameObject> tiles, List<Vector3> currentPositions, List<Vector3> targetPositions){
         float startTime = Time.time;
         float journeyLength = Vector3.Distance(currentPositions[0], targetPositions[0]);
-
 		float fractionOfJourney = 0;
-
         while(fractionOfJourney < 1){
 			float distCovered = (Time.time - startTime) * m_tilesSlideInSpeed;
 			fractionOfJourney = distCovered / journeyLength;
 			for(int i = 0; i < tiles.Count; i++){
 				tiles[i].transform.position = Vector3.Lerp(currentPositions[i], targetPositions[i], fractionOfJourney);
+			}
+			yield return null;
+		}
+	}
+
+	IEnumerator ZoomOutCamera(float targetSize){
+		print(targetSize);
+		float startTime = Time.time;
+		float currentSize = Camera.main.orthographicSize;
+		//float targetSize = Camera.main.orthographicSize + (m_worldSizeIncrease * 2);
+		float journeyLength = targetSize - currentSize;
+		float fractionOfJourney = 0;
+		while(fractionOfJourney < 1){
+			float distCovered = (Time.time - startTime) * m_cameraZoomOutSpeed;
+			fractionOfJourney = distCovered / journeyLength;
+			Camera.main.orthographicSize = Mathf.LerpUnclamped(currentSize, targetSize, m_cameraZoomOutCurve.Evaluate(fractionOfJourney));
+			yield return null;
+		}
+	}
+
+	IEnumerator SlideAndZoom(List<GameObject> tiles, List<Vector3> currentPositions, List<Vector3> targetPositions){
+		float startTime = Time.time;
+        float journeyLength = Vector3.Distance(currentPositions[0], targetPositions[0]);
+		float currentCamSize = Camera.main.orthographicSize;
+		float fractionOfJourney = 0;
+
+		bool resizeCamera = false;
+		float targetCamSize = 0;
+		if(m_worldCurrentSize >= (Camera.main.orthographicSize * 2)){
+			resizeCamera = true;
+			targetCamSize = m_worldCurrentSize/2 + 1;
+		}
+        while(fractionOfJourney < 1){
+			float distCovered = (Time.time - startTime) * m_tilesSlideInSpeed;
+			fractionOfJourney = distCovered / journeyLength;
+			for(int i = 0; i < tiles.Count; i++){
+				tiles[i].transform.position = Vector3.Lerp(currentPositions[i], targetPositions[i], fractionOfJourney);
+			}
+			
+			if(resizeCamera){
+				Camera.main.orthographicSize = Mathf.LerpUnclamped(currentCamSize, targetCamSize, m_cameraZoomOutCurve.Evaluate(fractionOfJourney));
 			}
 			yield return null;
 		}
